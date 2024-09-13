33abaddb6f4441b45680e93366e5ab6908411293
ArditaUka <ardita.uka@studentet.uni-pr.edu>
2024-09-13 17:15:50 +0200

    clean the code

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Mail\OrderInvoiceMail;
 use App\Services\OrderService;
-use GuzzleHttp\Client;
-use Barryvdh\DomPDF\Facade\Pdf;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Storage;
 
 class OrderController extends Controller
 {
